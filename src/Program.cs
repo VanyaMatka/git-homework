a09dd35b5781d679d342e08eb7f7d90f8dab647b
VanyaMatka <xzotow@yandex.ru>
2024-09-24 07:20:53 +0500

    Добавлен файл Program.cs с функцией для вычисления куба числа

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace GitHomework
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("¬ведите число:");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double cube = CalculateCube(number);
+            Console.WriteLine($" уб числа {number} равен {cube}");
+        }
+
+        static double CalculateCube(double num)
+        {
+            return Math.Pow(num, 3);
+        }
+    }
+}
